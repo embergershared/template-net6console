c8be5e719e0a71a6ba2919ed941b6b3b501dd325
Emmanuel <eb@mngenvmcap446692.onmicrosoft.com>
2023-11-16 14:30:15 -0500

    unwired ShowInfos

--- src/ConsoleApp/ConsoleApp/Program.cs
+++ src/ConsoleApp/ConsoleApp/Program.cs
@@ -8,48 +8,48 @@ using Console = ConsoleApp.Classes.Console;
 
 namespace ConsoleApp
 {
-    internal class Program
+  internal class Program
+  {
+    private static async Task Main(string[] args)
     {
-        private static async Task Main(string[] args)
-        {
-            #region Initialization
-            // Configuration: Managed by Host.CreateDefaultBuilder
-            var host = CreateHostBuilder(args).Build();
-            #endregion
+      #region Initialization
+      // Configuration: Managed by Host.CreateDefaultBuilder
+      var host = CreateHostBuilder(args).Build();
+      #endregion
 
-            var logger = host.Services.GetRequiredService<ILogger<Program>>();
-            logger.LogInformation("Program started");
+      // Adding logger and logging 
+      var logger = host.Services.GetRequiredService<ILogger<Program>>();
+      logger.LogInformation($"Main({args}) started");
 
-            var console = host.Services.GetRequiredService<IConsole>();
-            
-            _ = await console.RunAsync();
+      // var console = host.Services.GetRequiredService<IConsole>();
+      // _ = await console.RunAsync();
 
-            //await host.RunAsync();
+      //await host.RunAsync();
 
-            logger.LogInformation("Program finished");
-        }
+      logger.LogInformation($"Main({args}) finished");
+    }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
             // Ref: https://learn.microsoft.com/en-us/dotnet/api/microsoft.extensions.hosting.host.createdefaultbuilder?view=dotnet-plat-ext-7.0
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((_, services) =>
                 {
-                    services
-                        .AddTransient<IConsole, Console>()
-                        .AddTransient<IShowInfos, ShowInfos>();
+                  services
+                      // .AddTransient<IShowInfos, ShowInfos>()
+                      .AddTransient<IConsole, Console>();
                 })
                 .ConfigureLogging((_, logging) =>
                 {
-                    logging.ClearProviders();  // Clear existing providers
-                    
-                    // Add log providers:
-                    logging.AddSimpleConsole(options =>
-                    {
-                        // Ref: https://learn.microsoft.com/en-us/dotnet/core/extensions/console-log-formatter
-                        options.IncludeScopes = true;
-                        options.SingleLine = true;
-                        options.TimestampFormat = "yyy-MM-dd HH:mm:ss.fff ";
-                    });
+                  logging.ClearProviders();  // Clear existing providers
+
+                  // Add log providers:
+                  logging.AddSimpleConsole(options =>
+                  {
+                    // Ref: https://learn.microsoft.com/en-us/dotnet/core/extensions/console-log-formatter
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "yyy-MM-dd HH:mm:ss.fff ";
+                  });
                 });
-    }
+  }
 }
